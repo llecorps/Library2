4ecd6225662a39fc7a2236593f95266b6b46af1c
Lecorps <laurent.lecorps@gmail.com>
2019-09-01 17:25:22 +0200

    Test ListRecall

--- biblio/biblio-business/src/test/java/org/lle/biblio/business/impl/manager/UtlisateurMangerImplTest.java
+++ biblio/biblio-business/src/test/java/org/lle/biblio/business/impl/manager/UtlisateurMangerImplTest.java
@@ -7,9 +7,13 @@ import org.junit.runner.RunWith;
 import org.lle.biblio.model.bean.utilisateur.Utilisateur;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by TheAdmin on 01.09.2019.
@@ -20,6 +24,9 @@ public class UtlisateurMangerImplTest {
     //private UtilisateurManagerImpl manager;
     @Mock
     private Utilisateur pUtilisateur;
+    @Spy
+    List<Utilisateur> listRecall = new ArrayList<Utilisateur>();
+    private Utilisateur user = new Utilisateur();;
 
     @Before
     public void setUp() throws Exception {
@@ -28,6 +35,15 @@ public class UtlisateurMangerImplTest {
                 = new ClassPathXmlApplicationContext("classpath:/bootstrapContext.xml");
 
         //manager = vApplicationContext.getBean("UtilisateurManagerImpl", UtilisateurManagerImpl.class);
+
+        //Utilisateur user = new Utilisateur();
+        user.setRecall(true);
+        user.setPhone("0908067474");
+        user.setAdress("10 downing street");
+        user.setPassword("passwd");
+        user.setId(3);
+        user.setLogin("root");
+        user.setEmail("root.mail");
 
     }
 
@@ -38,6 +54,13 @@ public class UtlisateurMangerImplTest {
         Mockito.lenient().when(pUtilisateur.getLogin()).thenReturn("passwd");
 
         Assert.assertNotNull(pUtilisateur);
+
+    }
+
+    @Test
+    public void listRecall() {
+
+        Assert.assertTrue(listRecall.add(user));
 
     }
 
